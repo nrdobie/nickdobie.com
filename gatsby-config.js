71e407ba048c75f17e9eaf1d514676a3ba739828
Nicholas Dobie <nick@dobie.me>
2019-04-09 12:29:59 -0500

    updated a few settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require("http-proxy-middleware")
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Nick Dobie | Web Developer',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'I am a web developer from Minneapolis that enjoys expirimenting with new technologies.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
